f93764bf98402586e9202e0e89273dd5268a88f9
oms2304 <oms23@njit.edu>
2026-02-04 16:12:20 -0500

    Frontend + backend upload

--- Image-Gen-Model/main.py
+++
@@ -1,7 +0,0 @@
-from gui import ImageGeneratorApp
-import tkinter as tk
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = ImageGeneratorApp(root)
-    root.mainloop()
--- drafts/deprecated/automat_model.py
+++
@@ -1,7 +0,0 @@
-
-from pydantic     import BaseModel, ConfigDict
-from langchain_core.prompts.base import BasePromptTemplate
-
-class AutomatModel(BaseModel):
-    prompt: BasePromptTemplate
-    model_config = ConfigDict(arbitrary_types_allowed=True)
