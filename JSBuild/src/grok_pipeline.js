d5119a8b40e5130d62994294f3dce3ee6b79a0e4
Miles J. Litteral <Mandaloe2@gmail.com>
2026-03-09 05:11:57 -0400

    Create grok_pipeline.js

---
+++ JSBuild/src/grok_pipeline.js
@@ -0,0 +1,49 @@
+import { createXai } from '@ai-sdk/xai';
+import { generateText } from 'ai';
+
+//Certain models also support Structured Outputs, 
+// which allows you to enforce a schema for the LLM output. 
+// For an in-depth guide about using Grok for text responses, c
+// heck out the xAI Text Generation Guide.
+
+//User must provide key via UI to use Grok.
+const xai = createXai({ apiKey: process.env.XAI_API_KEY });
+
+async function get_grok_response(model, message){
+        let text = await generateText({
+            model: xai.responses(model),
+            system: 'You are Grok, a highly intelligent, helpful AI assistant.',
+            prompt: message,
+        });
+        return text
+    }
+
+async function get_grok_response_with_system_prompt(model, message){
+    let text = await generateText({
+            model: xai.responses(model),
+            system: 'You are Grok, a highly intelligent, helpful AI assistant.',
+            prompt: message,
+        });
+        return text
+    } 
+
+async function get_grok_image_response(model, message, image_link){
+//Grok can accept both text and images as input. Pass an image URL alongside your prompt:
+    const { text } = await generateText({
+        model: xai.responses(model),
+        messages: [{
+            role: 'user',
+            content: [
+                    { type: 'image', image: image_link},
+                    { type: 'text',  text:  message   },
+                ],
+            }],
+        });
+        return text;
+    }
+
+module.exports = {
+    get_grok_response,
+    get_grok_response_with_system_prompt,
+    get_grok_image_response
+}
